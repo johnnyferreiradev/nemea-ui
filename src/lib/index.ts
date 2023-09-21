1886475c0f06fa3f60f253cf6d8363a0c79409f1
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2023-09-21 10:42:31 -0300

    Test 1 - Two components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,67 +4,67 @@ export * from './locales';
 
 export { Button } from './components/Button';
 
-export { Alert } from './components/Alert';
+// export { Alert } from './components/Alert';
 
-export { Avatar } from './components/Avatar';
+// export { Avatar } from './components/Avatar';
 
-export { default as Image } from './components/Image';
+// export { default as Image } from './components/Image';
 
-export { default as ImagePlaceholder } from './components/ImagePlaceholder';
+// export { default as ImagePlaceholder } from './components/ImagePlaceholder';
 
-export { Toaster } from './components/Toaster';
+// export { Toaster } from './components/Toaster';
 
-export { Dropdown } from './components/Dropdown';
+// export { Dropdown } from './components/Dropdown';
 
-export { Form } from './components/Form';
+// export { Form } from './components/Form';
 
-export { Select } from './components/Select';
+// export { Select } from './components/Select';
 
-export { default as Checkbox } from './components/Checkbox';
+// export { default as Checkbox } from './components/Checkbox';
 
 export { default as Switch } from './components/Switch';
 
-export { RadioGroup } from './components/RadioGroup';
+// export { RadioGroup } from './components/RadioGroup';
 
-export { Modal } from './components/Modal';
+// export { Modal } from './components/Modal';
 
-export { default as Loader } from './components/Loader';
+// export { default as Loader } from './components/Loader';
 
-export { AlertDialog } from './components/AlertDialog';
+// export { AlertDialog } from './components/AlertDialog';
 
-export { Accordion } from './components/Accordion';
+// export { Accordion } from './components/Accordion';
 
-export { Collapsible } from './components/Collapsible';
+// export { Collapsible } from './components/Collapsible';
 
-export { PageLoader } from './components/PageLoader';
+// export { PageLoader } from './components/PageLoader';
 
-export { Popover } from './components/Popover';
+// export { Popover } from './components/Popover';
 
-export { default as Label } from './components/Label';
+// export { default as Label } from './components/Label';
 
-export { Tooltip } from './components/Tooltip';
+// export { Tooltip } from './components/Tooltip';
 
-export { Progress } from './components/Progress';
+// export { Progress } from './components/Progress';
 
-export { Tabs } from './components/Tabs';
+// export { Tabs } from './components/Tabs';
 
-export { default as Separator } from './components/Separator';
+// export { default as Separator } from './components/Separator';
 
-export { default as Skeleton } from './components/Skeleton';
+// export { default as Skeleton } from './components/Skeleton';
 
-export { default as Badge } from './components/Badge';
+// export { default as Badge } from './components/Badge';
 
-export { Range } from './components/Range';
+// export { Range } from './components/Range';
 
-export { Table } from './components/Table';
+// export { Table } from './components/Table';
 
-export { default as UploadArea } from './components/UploadArea';
+// export { default as UploadArea } from './components/UploadArea';
 
-export { default as Text } from './components/Text';
+// export { default as Text } from './components/Text';
 
-export { default as Input } from './components/Input';
+// export { default as Input } from './components/Input';
 
-export { default as Textarea } from './components/Textarea';
+// export { default as Textarea } from './components/Textarea';
 
-export { default as Calendar } from './components/Calendar';
-export type { DateRange } from './components/Calendar/types';
+// export { default as Calendar } from './components/Calendar';
+// export type { DateRange } from './components/Calendar/types';
