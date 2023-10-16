2ae9df5b0446636f7f9102a3b8183b78affc1a8a
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2023-10-16 12:14:59 -0300

    Adjusting alert dark theme

--- src/lib/components/Alert/AlertRoot/index.tsx
+++ src/lib/components/Alert/AlertRoot/index.tsx
@@ -18,6 +18,12 @@ const alertRoot = tv({
         '[&>.au-alert-content>.au-alert-title]:text-success-800',
         '[&>.au-alert-content>.au-alert-description]:text-success-800',
         '[&>.au-alert-close>.au-alert-close-button]:text-success-800',
+
+        'dark:bg-grayscale-800 dark:border-grayscale-800',
+        'dark:[&>.au-alert-icon]:text-success-300',
+        'dark:[&>.au-alert-content>.au-alert-title]:text-success-300',
+        'dark:[&>.au-alert-content>.au-alert-description]:text-success-300',
+        'dark:[&>.au-alert-close>.au-alert-close-button]:text-success-300',
       ],
       failure: [
         'bg-failure-200 border border-solid border-failure-200',
@@ -25,6 +31,12 @@ const alertRoot = tv({
         '[&>.au-alert-content>.au-alert-title]:text-failure-800',
         '[&>.au-alert-content>.au-alert-description]:text-failure-800',
         '[&>.au-alert-close>.au-alert-close-button]:text-failure-800',
+
+        'dark:bg-grayscale-800 dark:border-grayscale-800',
+        'dark:[&>.au-alert-icon]:text-failure-300',
+        'dark:[&>.au-alert-content>.au-alert-title]:text-failure-300',
+        'dark:[&>.au-alert-content>.au-alert-description]:text-failure-300',
+        'dark:[&>.au-alert-close>.au-alert-close-button]:text-failure-300',
       ],
       warning: [
         'bg-warning-200 border border-solid border-warning-200',
@@ -32,6 +44,12 @@ const alertRoot = tv({
         '[&>.au-alert-content>.au-alert-title]:text-warning-800',
         '[&>.au-alert-content>.au-alert-description]:text-warning-800',
         '[&>.au-alert-close>.au-alert-close-button]:text-warning-800',
+
+        'dark:bg-grayscale-800 dark:border-grayscale-800',
+        'dark:[&>.au-alert-icon]:text-warning-300',
+        'dark:[&>.au-alert-content>.au-alert-title]:text-warning-300',
+        'dark:[&>.au-alert-content>.au-alert-description]:text-warning-300',
+        'dark:[&>.au-alert-close>.au-alert-close-button]:text-warning-300',
       ],
     },
   },
