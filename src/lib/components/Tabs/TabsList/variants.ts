1cbac60b639c8d87f37f6c54983741ed030a1e2a
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2024-01-04 09:15:24 -0300

    Fixing tabs disabled themes

--- src/lib/components/Tabs/TabsList/variants.ts
+++ src/lib/components/Tabs/TabsList/variants.ts
@@ -10,77 +10,77 @@ export const tabsListVariants = tv({
       default: [
         'bg-none',
         '[&>.au-tabs-trigger]:bg-none [&>.au-tabs-trigger]:text-grayscale-400', // enabled
-        '[&>button.au-tabs-trigger[data-disabled]]:text-grayscale-200', // disabled
+        '[&>button.au-tabs-trigger[data-disabled]]:opacity-30 [&>button.au-tabs-trigger[data-disabled]]:pointer-events-none', // disabled
         '[&>.au-tabs-trigger:hover]:text-dark', // hover
         '[&>.au-tabs-trigger[data-state="active"]]:bg-grayscale-100 [&>.au-tabs-trigger[data-state="active"]]:text-dark', // selected
         // disabled and selected
-        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:text-grayscale-400',
+        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:opacity-30',
 
         // darkmode
-        '[&>button.au-tabs-trigger[data-disabled]]:dark:text-grayscale-700', // disabled
+        '[&>button.au-tabs-trigger[data-disabled]]:dark:opacity-30 [&>button.au-tabs-trigger[data-disabled]]:dark:pointer-events-none', // disabled
         '[&>.au-tabs-trigger:hover]:dark:text-light', // hover
         '[&>.au-tabs-trigger[data-state="active"]]:dark:bg-grayscale-800 [&>.au-tabs-trigger[data-state="active"]]:dark:text-light', // selected
         // disabled and selected
-        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:dark:text-grayscale-700',
+        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:dark:opacity-30',
       ],
       light: [
         'bg-grayscale-50',
         '[&>.au-tabs-trigger]:bg-none [&>.au-tabs-trigger]:text-grayscale-400', // enabled
-        '[&>button.au-tabs-trigger[data-disabled]]:bg-none [&>button.au-tabs-trigger[data-disabled]]:text-grayscale-200', // disabled
+        '[&>button.au-tabs-trigger[data-disabled]]:opacity-30 [&>button.au-tabs-trigger[data-disabled]]:pointer-events-none', // disabled
         '[&>.au-tabs-trigger:hover]:text-dark', // hover
         '[&>.au-tabs-trigger[data-state="active"]]:bg-light [&>.au-tabs-trigger[data-state="active"]]:text-dark', // selected
         // disabled and selected
-        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:bg-grayscale-50 [&>button.au-tabs-trigger[data-disabled][data-state="active"]]:text-grayscale-400',
+        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:opacity-30 [&>button.au-tabs-trigger[data-disabled][data-state="active"]]:text-grayscale-400',
 
         // darkmode
         'dark:bg-grayscale-800',
         '[&>.au-tabs-trigger]:bg-none [&>.au-tabs-trigger]:dark:text-grayscale-400', // enabled
-        '[&>button.au-tabs-trigger[data-disabled]]:dark:text-grayscale-700', // disabled
+        '[&>button.au-tabs-trigger[data-disabled]]:dark:opacity-30 [&>button.au-tabs-trigger[data-disabled]]:dark:pointer-events-none', // disabled
         '[&>.au-tabs-trigger:hover]:dark:text-light', // hover
         '[&>.au-tabs-trigger[data-state="active"]]:dark:bg-grayscale-700 [&>.au-tabs-trigger[data-state="active"]]:dark:text-light', // selected
         // disabled and selected
-        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:dark:bg-grayscale-700 [&>button.au-tabs-trigger[data-disabled][data-state="active"]]:dark:text-grayscale-400',
+        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:dark:opacity-30 [&>button.au-tabs-trigger[data-disabled][data-state="active"]]:dark:text-grayscale-500',
       ],
       dark: [
-        'bg-grayscale-950',
+        'bg-grayscale-800',
         '[&>.au-tabs-trigger]:bg-none [&>.au-tabs-trigger]:text-grayscale-400', // enabled
-        '[&>button.au-tabs-trigger[data-disabled]]:bg-none [&>button.au-tabs-trigger[data-disabled]]:text-grayscale-700', // disabled
+        '[&>button.au-tabs-trigger[data-disabled]]:opacity-30 [&>button.au-tabs-trigger[data-disabled]]:pointer-events-none', // disabled
         '[&>.au-tabs-trigger:hover]:text-light', // hover
         '[&>.au-tabs-trigger[data-state="active"]]:bg-grayscale-700 [&>.au-tabs-trigger[data-state="active"]]:text-light', // selected
         // disabled and selected
-        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:bg-grayscale-700 [&>button.au-tabs-trigger[data-disabled][data-state="active"]]:text-grayscale-400',
+        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:opacity-30 [&>button.au-tabs-trigger[data-disabled][data-state="active"]]:text-grayscale-500',
 
         // darkmode
         'dark:bg-grayscale-50',
-        '[&>button.au-tabs-trigger[data-disabled]]:dark:text-grayscale-50', // disabled
+        '[&>button.au-tabs-trigger[data-disabled]]:dark:opacity-30 [&>button.au-tabs-trigger[data-disabled]]:dark:pointer-events-none', // disabled
         '[&>.au-tabs-trigger:hover]:dark:text-dark', // hover
         '[&>.au-tabs-trigger[data-state="active"]]:dark:bg-light [&>.au-tabs-trigger[data-state="active"]]:dark:text-dark', // selected
         // disabled and selected
-        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:dark:bg-grayscale-50 [&>button.au-tabs-trigger[data-disabled][data-state="active"]]:dark:text-grayscale-400',
+        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:dark:opacity-30 [&>button.au-tabs-trigger[data-disabled][data-state="active"]]:dark:text-grayscale-400',
       ],
       darkUnderline: [
         'bg-none [&>.au-tabs-trigger]:rounded-none',
         '[&>.au-tabs-trigger]:bg-none [&>.au-tabs-trigger]:text-grayscale-400', // enabled
-        '[&>button.au-tabs-trigger[data-disabled]]:text-grayscale-50', // disabled
+        '[&>button.au-tabs-trigger[data-disabled]]:opacity-30 [&>button.au-tabs-trigger[data-disabled]]:pointer-events-none', // disabled
         '[&>.au-tabs-trigger:hover]:text-dark', // hover
         // selected
         '[&>.au-tabs-trigger[data-state="active"]]:border-x-transparent [&>.au-tabs-trigger[data-state="active"]]:border-t-transparent',
         '[&>.au-tabs-trigger[data-state="active"]]:border-b-dark [&>.au-tabs-trigger[data-state="active"]]:text-dark',
         // disabled and selected
-        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:text-grayscale-400 [&>button.au-tabs-trigger[data-disabled][data-state="active"]]:border-b-grayscale-300',
+        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:opacity-30 [&>button.au-tabs-trigger[data-disabled][data-state="active"]]:border-b-grayscale-300',
 
         // darkmode
-        '[&>button.au-tabs-trigger[data-disabled]]:dark:text-grayscale-700', // disabled
+        '[&>button.au-tabs-trigger[data-disabled]]:dark:opacity-30 [&>button.au-tabs-trigger[data-disabled]]:dark:pointer-events-none', // disabled
         '[&>.au-tabs-trigger:hover]:dark:text-light', // hover
         // selected
         '[&>.au-tabs-trigger[data-state="active"]]:dark:border-b-light [&>.au-tabs-trigger[data-state="active"]]:dark:text-light',
         // disabled and selected
-        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:dark:text-grayscale-700 [&>button.au-tabs-trigger[data-disabled][data-state="active"]]:border-b-grayscale-500',
+        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:dark:opacity-30 [&>button.au-tabs-trigger[data-disabled][data-state="active"]]:border-b-grayscale-500',
       ],
       primaryUnderline: [
         'bg-none [&>.au-tabs-trigger]:rounded-none',
         '[&>.au-tabs-trigger]:bg-none [&>.au-tabs-trigger]:text-grayscale-400', // enabled
-        '[&>button.au-tabs-trigger[data-disabled]]:text-grayscale-50', // disabled
+        '[&>button.au-tabs-trigger[data-disabled]]:opacity-30 [&>button.au-tabs-trigger[data-disabled]]:pointer-events-none', // disabled
         '[&>.au-tabs-trigger:hover]:text-primary-600', // hover
         // selected
         '[&>.au-tabs-trigger[data-state="active"]]:border-x-transparent [&>.au-tabs-trigger[data-state="active"]]:border-t-transparent',
@@ -89,12 +89,12 @@ export const tabsListVariants = tv({
         '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:text-grayscale-400 [&>button.au-tabs-trigger[data-disabled][data-state="active"]]:border-b-grayscale-300',
 
         // darkmode
-        '[&>button.au-tabs-trigger[data-disabled]]:dark:text-grayscale-700', // disabled
+        '[&>button.au-tabs-trigger[data-disabled]]:dark:opacity-30 [&>button.au-tabs-trigger[data-disabled]]:dark:pointer-events-none', // disabled
         '[&>.au-tabs-trigger:hover]:dark:text-primary-300', // hover
         // selected
         '[&>.au-tabs-trigger[data-state="active"]]:dark:border-b-primary-600 [&>.au-tabs-trigger[data-state="active"]]:dark:text-primary-300',
         // disabled and selected
-        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:dark:text-grayscale-700 [&>button.au-tabs-trigger[data-disabled][data-state="active"]]:border-b-grayscale-500',
+        '[&>button.au-tabs-trigger[data-disabled][data-state="active"]]:dark:opacity-30 [&>button.au-tabs-trigger[data-disabled][data-state="active"]]:border-b-grayscale-500',
       ],
     },
     rounded: {
