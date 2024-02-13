fc1c53351e3dc80cf5bb15871b52f5702582818a
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2024-02-13 14:30:20 -0300

    fix: dropdown and calendar

--- src/lib/components/Dropdown/DropdownRoot/types.ts
+++ src/lib/components/Dropdown/DropdownRoot/types.ts
@@ -1,9 +1,11 @@
+import { DropdownMenuProps } from '@radix-ui/react-dropdown-menu';
+
 enum DropdownThemes {
   light = 'light',
   dark = 'dark',
 }
 
-export interface DropdownProps {
+export interface DropdownProps extends DropdownMenuProps {
   children?: React.ReactNode;
   triggerAsChild?: boolean;
   trigger: React.ReactNode;
